bf52f0ef792806a5ac8e6dd1eaa6925f377fa922
raphaelbraganca <raphelbarreto@gmail.com>
2019-12-01 20:03:43 -0300

    Add Controller method (Organizacao)
    Adicionando métodos get (consulta) e post (cadastro) para manutenção de estabelecimentos. Retringindo tamanho dos campos.

---
+++ MyPayablesList_Stn/Controllers/OrganizacaoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyPayablesList_Stn.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyPayablesList_Stn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizacaoController : ControllerBase
+    {
+
+        private readonly PayablesAPIContext _context;
+
+        public OrganizacaoController(PayablesAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CadOrganizacao>>> GetOrgItem()
+        {
+            return await _context.CadOrganizacaoItens.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CadOrganizacao>> PostOrgItem(CadOrganizacao organizacao)
+        {
+            _context.CadOrganizacaoItens.Add(organizacao);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrgItem", new CadOrganizacao { OrgOrganizacaoId = organizacao.OrgOrganizacaoId }, organizacao);
+        }
+    }
+}
--- MyPayablesList_Stn/Models/CadOrganizacao.cs
+++ MyPayablesList_Stn/Models/CadOrganizacao.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyPayablesList_Stn.Models
 {
     public partial class CadOrganizacao
     {
         public Guid OrgOrganizacaoId { get; set; }
+
+        [StringLength(65, MinimumLength = 3)]
         public string OrgNome { get; set; }
+
+        [StringLength(35)]
         public string OrgCategoria { get; set; }
     }
 }
