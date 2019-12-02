5eb8e9fad98c4f3f87d9235f74c2ee1d07c905d1
raphaelbraganca <raphelbarreto@gmail.com>
2019-12-02 06:57:20 -0300

    Updating Lancaemento model properties.
    Adicionando configurações e validações para os campos da tabela de lançamento.

--- MyPayablesList_Stn/Models/FinLancamento.cs
+++ MyPayablesList_Stn/Models/FinLancamento.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyPayablesList_Stn.Models
 {
     public partial class FinLancamento
     {
         public int LanLancamentoId { get; set; }
+        [Required]
         public Guid LanOrgOrganizacaoId { get; set; }
+        [Required]
+        [StringLength(15)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string LanFormaPagamento { get; set; }
         public decimal? LanValorLancamento { get; set; }
         public string LanMoeda { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? LanDataLancamento { get; set; }
         public Guid? LanPesPessoaId { get; set; }
     }
