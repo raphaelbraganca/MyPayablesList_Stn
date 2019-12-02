3d337efea34ead93e491d312e28cff0bb0aed133
raphaelbraganca <raphelbarreto@gmail.com>
2019-12-02 06:54:01 -0300

    Add Get/Post methods for Lancamento
    Adicionanco controller com funções de consulta (get) e cadastro (post) de lançamentos financeiros.

---
+++ MyPayablesList_Stn/Controllers/LancamentoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPayablesList_Stn.Models;
+
+namespace MyPayablesList_Stn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LancamentoController : ControllerBase
+    {
+
+        private readonly PayablesAPIContext _context;
+        public LancamentoController(PayablesAPIContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FinLancamento>>> GetLanItens()
+        {
+            return await _context.FinLancamentoItens.ToListAsync();
+        }
+
+        [Consumes(MediaTypeNames.Application.Json)]
+        [HttpPost]
+        public async Task<ActionResult<FinLancamento>> PostLanItem(FinLancamento lancamento)
+        {
+            _context.FinLancamentoItens.Add(lancamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLanItens", new FinLancamento { LanLancamentoId = lancamento.LanLancamentoId }, lancamento);
+        }
+
+        }
+
+    }
